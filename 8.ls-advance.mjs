07500f39bf6fecf6ae3f35793a910d56119eabf6
unknown <f.gabrielvillalba@gmail.com>
2025-04-09 11:26:29 -0300

    git ignore

--- 8.ls-advance.mjs
+++ 8.ls-advance.mjs
@@ -1,14 +1,32 @@
 import fs from'node:fs/promises'
 import { join } from 'node:path'
+import pc from 'picocolors'
 
 const folder = process.argv[2] ?? '.'
+
+
+function colorBySize(size){
+  let color;
+  switch(true){
+    case size < 500:
+      color = "green";
+      break;
+    case size < 1000:
+      color = "yellow";
+      break;
+    default:
+      color = "red"; 
+      break;
+  }
+  return pc[color](size.toString().padStart(10))
+}
 
 async function ls(folder){
     let files
     try {
         files = await fs.readdir(folder)
     } catch(e) {
-        console.log(`No se pudo leer el directorio ${folder}`)
+        console.log(pc.red(`❌ No se pudo leer el directorio ${folder}`))
         process.exit(1)
     }
     
@@ -18,7 +36,7 @@ async function ls(folder){
       try{
         stats = await fs.stat(filePath)
       } catch(e){
-        console.log(`Error al obtener información del archivo: ${file}`)
+        console.log(pc.red(`❌ Error al obtener información del archivo: ${file}`))
         process.exit(1)
       }
 
@@ -27,7 +45,8 @@ async function ls(folder){
       const fileSize = stats.size
       const fileModified = stats.mtime.toLocaleString()
 
-      return `${fileType} ${file.padEnd(30)} ${fileSize.toString().padStart(10)} ${fileModified}` 
+      // return ` ${pc.white(fileType)} ${pc.blue(file.padEnd(30))} ${pc.green(fileSize.toString().padStart(10))} ${pc.yellow(fileModified)}` 
+      return ` ${pc.white(fileType)} ${pc.blue(file.padEnd(30))} ${colorBySize(fileSize)} ${pc.cyan(fileModified)}` 
     })
 
     const filesInfo = await Promise.all(filesPromises)
