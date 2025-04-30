8154445b21be7802985de3a97790485114feb167
unknown <f.gabrielvillalba@gmail.com>
2025-04-30 11:43:48 -0300

    update: models

--- clase-4/controllers/movies.js
+++ clase-4/controllers/movies.js
@@ -1,5 +1,5 @@
-import { MovieModel } from '../models/movie';
-import { validateMovie, validatePartialMovie } from '../schemas/movies';
+import { MovieModel } from '../models/locale-file-system/movie.js';
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js';
 
 export class MovieController {
   static async getAll(req, res) {
